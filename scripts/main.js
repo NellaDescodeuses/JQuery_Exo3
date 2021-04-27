3023d0193275fb094443b2616151763554d13ce8
NellaDescodeuses <nellaruscade.pro@gmail.com>
2021-04-27 11:12:28 +0200

    Ajout de l'exo

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+});
+$(document).ready(function() {
+    //Au clic sur le bouton,
+    $('button').click(function () { 
+        //ma div id="text" change de police
+        $('#text').css('font-family','courrier');
+    });
+});
